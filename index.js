5e55f1483e65ed9d45020452d838de949951c115
Moshe Weitzman <weitzman@tejasa.com>
2016-02-09 17:21:37 -0500

    Newer express syntax.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 var express = require('express');
 var port = process.env.PORT || 3000;
-var app = express.createServer();
+var express = require('express');
+var app = express();
 
 app.get('/', function(request, response) {
   response.sendfile(__dirname + '/index.html');
