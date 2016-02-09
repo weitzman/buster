c81009c0b54b055e3057441521f63e88d3ec4ab4
Moshe Weitzman <weitzman@tejasa.com>
2016-02-09 17:30:53 -0500

    More Express tries.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
-var express = require('express');
-var port = process.env.PORT || 3000;
+//var express = require('express');
+//var port = process.env.PORT || 3000;
+//var express = require('express');
+//var app = express();
+//
+//app.get('/', function(request, response) {
+//  response.sendfile(__dirname + '/index.html');
+//}).configure(function() {
+//  app.use('/images', express.static(__dirname + '/images'));
+//}).listen(port);
+
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.sendfile(__dirname + '/index.html');
-}).configure(function() {
-  app.use('/images', express.static(__dirname + '/images'));
-}).listen(port);
+app.listen(process.env.PORT || 3000);
