52504754d679e2ebf7f63268e03ff02f479a7b4c
Moshe Weitzman <weitzman@tejasa.com>
2016-02-09 16:47:24 -0500

    Make index.html servable via Express.

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express.createServer();
+
+app.get('/', function(request, response) {
+  response.sendfile(__dirname + '/index.html');
+}).configure(function() {
+  app.use('/images', express.static(__dirname + '/images'));
+}).listen(port);
